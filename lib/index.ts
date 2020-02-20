0c8e0089113e00994836fb1629edc8a6bcf4ab78
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-02-20 14:00:55 -0800

    TsServer said .testCoverage is not async

--- lib/index.ts
+++ lib/index.ts
@@ -4,18 +4,20 @@ export function setupXStateTest<TestContext, TContext>(
   hooks: NestedHooks,
   testModel: TestModel<TestContext, TContext>
 ) {
-  hooks.after(async function(assert) {
+  hooks.after(function(assert) {
     try {
-      await testModel.testCoverage();
+      testModel.testCoverage();
     } catch (error) {
       assert.ok(false, 'Not all tests were exercised in this test suite (see console)');
-      console.info([
-        'Tests in this file are generated. It is checking for full coverage of all possible test',
-        'paths. However, if you have filtered or scoped (with module=...) then this assertion will be',
-        'a false negative and is safe to ignore.',
-        '',
-        'For more details: https://github.com/davidkpiano/xstate/tree/master/packages/xstate-test#testmodeltestcoverageoptions'
-      ].join('\n'));
+      console.info(
+        [
+          'Tests in this file are generated. It is checking for full coverage of all possible test',
+          'paths. However, if you have filtered or scoped (with module=...) then this assertion will be',
+          'a false negative and is safe to ignore.',
+          '',
+          'For more details: https://github.com/davidkpiano/xstate/tree/master/packages/xstate-test#testmodeltestcoverageoptions',
+        ].join('\n')
+      );
       console.error(error);
     }
   });
