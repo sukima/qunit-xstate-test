8267edec256f13b1027537e5b2133fce2351dbc1
Devin Weaver <suki@tritarget.org>
2020-02-21 16:22:07 -0500

    WIP fill in xstate integration tests

--- tests/integration/fixtures/ember-3.16/test-app/tests/unit/qunit-xstate-test.ts
+++ tests/integration/fixtures/ember-3.16/test-app/tests/unit/qunit-xstate-test.ts
@@ -1,17 +1,63 @@
+import { Machine } from 'xstate';
+import { createModel } from '@xstate/test';
 import { module, test } from 'qunit';
 import sinon, { SinonSpy } from 'sinon';
-
 import { setupXStateTest } from 'qunit-xstate-test';
 
-module('Integration | setupXStateTest', function(hooks) {
+type MetaTestContext = { assert: Assert };
+
+const thingGoodModel = createModel(
+  Machine({
+    id: 'thing-good',
+    initial: 'step1',
+    states: {
+      'step1': {
+        on: { TOGGLE: 'step2' },
+        meta: { test: ({ assert }: MetaTestContext) => assert.ok(true) },
+      },
+      'step2': {
+        meta: { test: ({ assert }: MetaTestContext) => assert.ok(true) },
+      },
+    },
+  })
+);
+
+const thingBadModel = createModel(
+  Machine({
+    id: 'thing-bad',
+    initial: 'step1',
+    states: {
+      'step1': {
+        meta: { test: ({ assert }: MetaTestContext) => assert.ok(true) },
+      },
+      'step2': {
+        meta: { test: ({ assert }: MetaTestContext) => assert.ok(true) },
+      },
+    },
+  })
+);
+
+
+module('Integration | setupXStateTest', function() {
 
   module('Success', function(hooks) {
-    setupXStateTest(hooks, undefined);
+    setupXStateTest(hooks, thingGoodModel);
 
-    // TODO: Test Loop Here
+    thingGoodModel.getShortestPathPlans().forEach(plan => {
+      module(plan.description, function() {
+        plan.paths.forEach(path => {
+          test(path.description, function(assert) {
+            path.test({ assert });
+          });
+        });
+      });
+    });
+
   });
 
   module('Failure', function(hooks) {
+    setupXStateTest(hooks, thingBadModel);
+
     let okSpy: SinonSpy;
 
     hooks.before(function(assert) {
@@ -25,9 +71,16 @@ module('Integration | setupXStateTest', function(hooks) {
       assert.equal(okSpy.getCall(0).args[1], 'Not all tests were exercised in this test suite (see console)');
     });
 
-    setupXStateTest(hooks, undefined);
+    thingBadModel.getShortestPathPlans().forEach(plan => {
+      module(plan.description, function() {
+        plan.paths.forEach(path => {
+          test(path.description, function(assert) {
+            path.test({ assert });
+          });
+        });
+      });
+    });
 
-    // TODO: Test Loop Here
   });
 
 });
